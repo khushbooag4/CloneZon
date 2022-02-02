9d28a148b701bd5740308c40897c2b7e219fa907
Khushboo <kushbooagarwal3584@gmail.com>
2022-02-02 11:34:57 +0530

    JWT

--- Server/db/Database.js
+++ Server/db/Database.js
@@ -4,7 +4,6 @@ try {
     mongoose.connect(process.env.DATABASE_URL);
 
     var db = mongoose.connection;
-    //Bind connection to error event (to get notification of connection errors)
     db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 } catch (error) {
     console.log(error);
