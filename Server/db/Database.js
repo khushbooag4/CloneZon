d62d70715215b4ccd3d48ebfd5eac26a5dc9beaa
Khushboo <kushbooagarwal3584@gmail.com>
2022-02-20 22:23:52 +0530

    Screeshots Added

--- Server/db/Database.js
+++ Server/db/Database.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 try {
-    mongoose.connect(process.env.DATABASE_URL);
+    mongoose.connect("mongodb://localhost:27017/amazondb");
 
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'MongoDB connection error:'));
