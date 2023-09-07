ca33b8ffaf9909a1dff1b5245691e0b2126af587
Khushboo Agarwal <khushbooagarwal.cse24@jecrc.ac.in>
2023-09-07 21:42:34 +0530

    database link env

--- db/Database.js
+++ db/Database.js
@@ -1,12 +1,11 @@
 var mongoose = require("mongoose");
+require("dotenv").config();
 
 try {
-  mongoose.connect(
-    "mongodb+srv://khushboo:1234@onair.bb9ktly.mongodb.net/?retryWrites=true&w=majority", {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-    });
-
+  mongoose.connect(process.env.DATABASE_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
   var db = mongoose.connection;
   db.on("error", console.error.bind(console, "MongoDB connection error:"));
 } catch (error) {
