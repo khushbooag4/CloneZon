5246cc9dd2bbedccea7b0910634be7a0862aed5e
Khushboo Agarwal <khushbooagarwal.cse24@jecrc.ac.in>
2023-09-07 22:51:20 +0530

    vercel deploy

--- Server/db/Database.js
+++ Server/db/Database.js
@@ -2,10 +2,13 @@ var mongoose = require("mongoose");
 require("dotenv").config();
 
 try {
-  mongoose.connect(process.env.DATABASE_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
+  mongoose.connect(
+    "mongodb+srv://khushboo123:ukp123@cluster0.cfm7v.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  );
   var db = mongoose.connection;
   db.on("error", console.error.bind(console, "MongoDB connection error:"));
 } catch (error) {
